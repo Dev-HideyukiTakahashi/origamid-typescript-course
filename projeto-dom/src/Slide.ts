19ff273bd3d845a6404bc747ed42efe4fa671755
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2024-11-09 02:50:07 -0300

    aula: prev e next

--- projeto-dom/src/Slide.ts
+++ projeto-dom/src/Slide.ts
@@ -18,7 +18,7 @@ export default class Slide {
     this.index = 0;
     this.slide = this.slides[this.index];
 
-    this.show(this.index);
+    this.init();
   }
 
   hide(element: Element) {
@@ -31,5 +31,32 @@ export default class Slide {
     this.slides.forEach(el => this.hide(el));
     this.slide.classList.add('active');
   }
-}
 
+  private addControls() {
+    const prevButton = document.createElement("button");
+    const nextButton = document.createElement("button");
+    prevButton.innerText = "Slide Anterior";
+    nextButton.innerText = "Próximo Slide";
+
+    nextButton.addEventListener("pointerup", () => this.next())
+    prevButton.addEventListener("pointerup", () => this.prev())
+
+    this.controls.appendChild(prevButton);
+    this.controls.appendChild(nextButton);
+  }
+
+  prev() {
+    const prev = this.index > 0 ? this.index - 1 : this.slides.length - 1;
+    this.show(prev);
+  }
+
+  next() {
+    const next = (this.index + 1) < this.slides.length ? this.index + 1 : 0;
+    this.show(next);
+  }
+
+  private init() {
+    this.addControls();
+    this.show(this.index);
+  }
+}
